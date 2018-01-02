3b95b5fe702723b0c8d563baabcd154d6a45b4cb
jaberu <alexander.herr.81@gmail.com>
2018-01-02 07:44:29 +0100

    add assertion

--- iso19112/src/test/java/net/opengis/iso19112/ExamplesTest.java
+++ iso19112/src/test/java/net/opengis/iso19112/ExamplesTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by aherr on 14.06.2016.
  */
@@ -13,6 +15,7 @@ public class ExamplesTest {
     @Test
     public void testBoundaries() throws JAXBException {
         JAXBContext jaxb = JAXBContext.newInstance("net.opengis.iso19112:net.opengis.gml.v_3_1_1:net.opengis.iso19139.gmd.v_20070417:net.opengis.wfs.v_1_1_0");
-        jaxb.createUnmarshaller().unmarshal(getClass().getResourceAsStream("/examples/boundary.xml"));
+        Object jaxbObj = jaxb.createUnmarshaller().unmarshal(getClass().getResourceAsStream("/examples/boundary.xml"));
+        assertNotNull(jaxbObj);
     }
 }
